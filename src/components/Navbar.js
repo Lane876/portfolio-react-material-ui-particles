e502764fa2434cfa8661d2c79f4c14731d8b566d
Lane876 <milenkovicmilan10@gmail.com>
2020-04-12 19:40:41 +0200

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import MobileRightMenuSlider from "@material-ui/core/Drawer";
+import {
+  AppBar,
+  Toolbar,
+  ListItem,
+  IconButton,
+  ListItemText,
+  ListItemIcon,
+  Avatar,
+  Divider,
+  List,
+  Typography,
+  Box,
+} from "@material-ui/core";
+
+import {
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+} from "@material-ui/icons";
+import avatar from "../avatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#511",
+    height: "100vh",
+  },
+
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "tan",
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contact",
+  },
+];
+const Navbar = () => {
+  const [state, setState] = useState({
+    right: false,
+  });
+
+  const classes = useStyles();
+
+  const toggleSlider = (slider, open) => () => {
+    setState({ ...state, [slider]: open });
+  };
+
+  const sideList = (slider) => (
+    <Box
+      className={classes.menuSliderContainer}
+      component="div"
+      onClick={toggleSlider(slider, false)}
+    >
+      <Avatar className={classes.avatar} src={avatar} alt="Milan portfolio" />
+      <Divider />
+      <List>
+        {menuItems.map((lstItem, key) => (
+          <ListItem button key={key}>
+            <ListItemIcon className={classes.listItem}>
+              {lstItem.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              primary={lstItem.listText}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton onClick={toggleSlider("right", true)}>
+              <ArrowBack style={{ color: "tomato" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "tan" }}>
+              Portfolio
+            </Typography>
+            <MobileRightMenuSlider
+              anchor="right"
+              open={state.right}
+              onClose={toggleSlider("right", false)}
+            >
+              {sideList("right")}
+            </MobileRightMenuSlider>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+export default Navbar;
---
+++ src/components/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particlesStyle: {
+    position: "absolute",
+  },
+});
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesStyle}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              desity: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              strole: {
+                width: "1",
+                color: "tomato",
+              },
+            },
+            size: {
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 7,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                speed: 1,
+                opacity_min: 0.1,
+                enable: true,
+                sync: true,
+              },
+            },
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default Home;
